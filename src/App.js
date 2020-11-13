3acfa77e736249df5decfeb60ca5b636d63a5afd
Ahmed Amir Salem (Kenway The Great) <ahmad_amir@live.com>
2020-11-14 01:11:24 +0200

    Will add the necessary components.

--- src/App.js
+++ src/App.js
@@ -10,12 +10,72 @@ function App() {
   const [passwordError, setPasswordError] = useState("");
   const [hasAcc, setHasAcc] = useState(false);
 
+  const clearInputs = () => {
+    setEmail("");
+    setPassword("");
+  };
+
+  const clearErrors = () => {
+    setEmailError("");
+    setPasswordError("");
+  };
+
   const handleLogin = () => {
+    clearErrors();
     fire
       .auth()
       .signInWithEmailAndPassword(email, password)
-      .catch((err) => {console.log(err)});
+      .catch((err) => {
+        switch (err.code) {
+          case "auth/invalid-email":
+          case "auth/user-disabled":
+          case "auth/user-not-found":
+            setEmailError(err.message);
+            break;
+          case "auth/wrong-password":
+            setPasswordError(err.message);
+            break;
+        }
+      });
   };
+
+  const handleSignUp = () => {
+    clearErrors();
+    fire
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch((err) => {
+        switch (err.code) {
+          case "auth/email-already-in-use":
+          case "auth/invalid-email":
+            setEmailError(err.message);
+            break;
+          case "auth/wrong-password":
+            setPasswordError(err.message);
+            break;
+        }
+      });
+  };
+
+  const handleLogout = () => {
+    fire.auth.signOut();
+  };
+
+  const authListener = () => {
+    fire.auth.onAuthStateChanged((user) => {
+      if (user) {
+        clearInputs();
+        setUser(user);
+      } else {
+        setUser("");
+      }
+    });
+  };
+
+  useEffect(() => {
+    authListener();
+  }, []);
+
   return (
     <div className="App">
       <h1>Hell O' World</h1>
